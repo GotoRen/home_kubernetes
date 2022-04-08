23421fe5eae882970a9238cb44f182fafdd80a53
GotoRen <r0719en@pluslab.org>
2022-04-08 22:05:29 +0900

    🐛 [Update] Setting scripts.

--- node/scripts/reset_cluster.sh
+++ node/scripts/reset_cluster.sh
@@ -2,7 +2,12 @@
 
 kubeadm reset
 
+rm -rf /var/lib/cni
 rm -rf /etc/kubernetes/*
 rm -rf /var/lib/etcd/
 
-kill -9 $(lsof -t -i:10250)
+kill -9 $(sudo lsof -t -i:10250)
+
+systemctl daemon-reload
+systemctl restart kubelet
+systemctl status kubelet
